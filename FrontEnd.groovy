b1d0ef2229a59eb73b46ccb11875143e7c4257f2
Berger-DM <sargento.tainha@gmail.com>
2017-11-23 21:38:38 -0200

    Inicial
    Arquivos iniciais para o trabalho. Classe separada do script que executa seu construtor, assim podemos usar um script pra trabalhar com diversas classes.

---
+++ FrontEnd.groovy
@@ -0,0 +1,406 @@
+import groovy.swing.SwingBuilder
+import javax.swing.*
+import java.awt.BorderLayout
+
+
+class FrontEnd {
+    FrontEnd() {
+        def builder = new SwingBuilder()
+        builder.edt {
+            frame(title: 'Battleship', size: [500, 350], show: true, defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE) {
+                borderLayout(vgap: 5)
+
+                panel(constraints: BorderLayout.CENTER) {
+                    tableLayout {
+                        tr {
+                            td {
+                                label '  '
+                            }
+                            td {
+                                label ' A'
+                            }
+                            td {
+                                label ' B'
+                            }
+                            td {
+                                label ' C'
+                            }
+                            td {
+                                label ' D'
+                            }
+                            td {
+                                label ' E'
+                            }
+                            td {
+                                label ' F'
+                            }
+                            td {
+                                label ' G'
+                            }
+                            td {
+                                label ' H'
+                            }
+                            td {
+                                label ' I'
+                            }
+                            td {
+                                label ' J'
+                            }
+                        }
+                        tr {
+                            td {
+                                label '1'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                        tr {
+                            td {
+                                label '2'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                        tr {
+                            td {
+                                label '3'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                        tr {
+                            td {
+                                label '4'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                        tr {
+                            td {
+                                label '5'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                        tr {
+                            td {
+                                label '6'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                        tr {
+                            td {
+                                label '7'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                        tr {
+                            td {
+                                label '8'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                        tr {
+                            td {
+                                label '9'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                        tr {
+                            td {
+                                label '10'
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                            td {
+                                button(text: ' ')
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
