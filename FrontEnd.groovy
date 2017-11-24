05f92e024393c678ada4d47d5460b3984e318a37
Berger-DM <sargento.tainha@gmail.com>
2017-11-24 00:43:55 -0200

    Atualização
    Permite alteração do caractere/texto nos botões. Como será implementada a visualização do jogo.

--- FrontEnd.groovy
+++ FrontEnd.groovy
@@ -7,8 +7,9 @@ class FrontEnd {
     FrontEnd() {
         def builder = new SwingBuilder()
         builder.edt {
-            frame(title: 'Battleship', size: [500, 350], show: true, locationRelativeTo: null, resizable: false, 
-                  defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE) {
+            frame(title: 'Battleship', size: [500, 350], show: true, locationRelativeTo: null, resizable: false,
+                    defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE) {
+
                 borderLayout(vgap: 5)
 
                 panel(constraints: BorderLayout.CENTER) {
@@ -53,36 +54,36 @@ class FrontEnd {
                                 label '1'
                             }
                             td {
-                                button(text: ' ')
+                                a1 = button text:' ', actionPerformed: {a1.setText("."); a1.setEnabled(false)}
                             }
                             td {
-                                button(text: ' ')
+                                b1 = button text:' ', actionPerformed: {b1.setText("."); b1.setEnabled(false)}
                             }
                             td {
-                                button(text: ' ')
+                                c1 = button text:' ', actionPerformed: {c1.setText("."); c1.setEnabled(false)}
                             }
                             td {
-                                button(text: ' ')
+                                d1 = button text:' ', actionPerformed: {d1.setText("."); d1.setEnabled(false)}
                             }
                             td {
-                                button(text: ' ')
+                                e1 = button text:' ', actionPerformed: {e1.setText("."); e1.setEnabled(false)}
                             }
                             td {
-                                button(text: ' ')
+                                f1 = button text:' ', actionPerformed: {f1.setText("."); f1.setEnabled(false)}
                             }
                             td {
-                                button(text: ' ')
+                                g1 = button text:' ', actionPerformed: {g1.setText("."); g1.setEnabled(false)}
                             }
                             td {
-                                button(text: ' ')
+                                h1 = button text:' ', actionPerformed: {h1.setText("."); h1.setEnabled(false)}
                             }
                             td {
-                                button(text: ' ')
+                                i1 = button text:' ', actionPerformed: {i1.setText("."); i1.setEnabled(false)}
                             }
                             td {
-                                button(text: ' ')
+                                j1 = button text:' ', actionPerformed: {j1.setText("."); j1.setEnabled(false)}
                             }
-                        }
+                        } // Terminar de configurar por essa linha e então copiar para as demais.
                         tr {
                             td {
                                 label '2'
